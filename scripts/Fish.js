1f3cffeec3c5896126eaffbaaadef58428bbded8
Susan Wiens <susanmichellewiens@gmail.com>
2020-10-08 13:01:45 -0500

    created fish.js

---
+++ scripts/Fish.js
@@ -0,0 +1,11 @@
+export const Fish = (fishObj) => {
+    return `<div class="fish">
+    <div>Name: ${fishObj.name}</div>
+    <img src="${fishObj.image}" alt="">
+    <div>Scientific Name:${fishObj.scientific}</div>
+    <div>Size:${fishObj.size}</div>
+    <div>Harvest Location:${fishObj.location}</div>
+    <div>Diet: ${fishObj.food}</div>
+<div>     
+    `
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,6 @@
-import { useFish } from './FishDataProvider.js'
+
 import { FishList } from './FishList.js'
 
 
-const allTheFish = useFish()
 
 FishList()
