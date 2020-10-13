adb7e7e46f21898ef2a14c6783ba9243ae5f21a6
Susan Wiens <susanmichellewiens@gmail.com>
2020-10-12 19:22:13 -0500

    add filtering-incomplete

--- scripts/fish/FishDataProvider.js
+++ scripts/fish/FishDataProvider.js
@@ -41,4 +41,28 @@ export const useFish = () => {
 export const addFish = (fishObject) => {
     fishCollection.push(fishObject)
 }
-   
+  
+
+export const mostHolyFish = () => {
+    // 3, 6, 9, 12, etc... fish
+    const holyFish = []
+
+    for (const fish of fishCollection) {
+         if(fish.length % 3 ===0) {
+            holyFish.push(fish)
+         }
+         
+    }
+
+    return holyFish
+}
+
+export const soldierFish = () => {
+    // 5, 10, 15, 20, 25, etc... fish
+    return soldiers
+}
+
+export const nonHolyFish = () => {
+    // Any fish not a multiple of 3 or 5
+    return regularFish
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,16 @@
 
+import { mostHolyFish } from './fish/FishDataProvider.js'
 import { FishList } from './fish/FishList.js'
 
 FishList()
+
+const numbers = [4,1,5,8,6,4,2,2,4,6,7,7,8,9,7,5,3,2,1,3,4,2,1,3,5,7,76,5,9,4,23,5,3,2,6,8,98,65,3,1,3,4]
+
+// Only display even numbers
+for (const number of numbers) {
+    if (number % 2 === 0) {
+        console.log(number)
+    }
+}
+
+mostHolyFish()
